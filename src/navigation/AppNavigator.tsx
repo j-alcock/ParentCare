580c17447741981deb1f93c156c0241afead0776
Revinicus <49887098+Revinicus@users.noreply.github.com>
2025-10-30 09:37:53 -0700

    Medications: add prescription fields (refills, refill date, timing, with food) and UI; tests still passing

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { enableScreens } from 'react-native-screens';
+
 import ContactsScreen from '../screens/ContactsScreen';
 import HealthcareScreen from '../screens/HealthcareScreen';
 import CalendarScreen from '../screens/CalendarScreen';
 import CommunicationScreen from '../screens/CommunicationScreen';
+
+enableScreens(false);
+
 const Tab = createBottomTabNavigator();
+
 const AppNavigator = () => (
   <NavigationContainer>
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: true,
+      }}
+    >
       <Tab.Screen name="Contacts" component={ContactsScreen} />
       <Tab.Screen name="Healthcare" component={HealthcareScreen} />
       <Tab.Screen name="Calendar" component={CalendarScreen} />
@@ -16,4 +26,5 @@ const AppNavigator = () => (
     </Tab.Navigator>
   </NavigationContainer>
 );
+
 export default AppNavigator;
